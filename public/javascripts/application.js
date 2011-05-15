f9c8880e7f022fb8489060b9ee1c6fd4bc7e2e2f
Sam Charrington <sam@charrington.com>
2011-05-15 10:19:56 -0500

    Added user microposts

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+
+  $('#micropost_content').keyup(function() {
+    var left = 140 - $(this).val().length;
+    if (left < 0) {
+      left = 0;
+    }
+    $('#counter').text(left);
+  });
+
+});
