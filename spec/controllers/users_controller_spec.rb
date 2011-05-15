b6cd4481c06666247801487326a851d8686dc29a
Sam Charrington <sam@charrington.com>
2011-05-15 14:22:35 -0500

    Update tests for microposts code following refactor

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -107,8 +107,8 @@ describe UsersController do
       mp1 = Factory(:micropost, :user => @user, :content => 'Foo bar')
       mp2 = Factory(:micropost, :user => @user, :content => 'Baz quux')
       get :show, :id => @user
-      response.should have_selector('span.content', :content => mp1.content)
-      response.should have_selector('span.content', :content => mp2.content)
+      response.should have_selector('.content', :content => mp1.content)
+      response.should have_selector('.content', :content => mp2.content)
     end
   end
 
